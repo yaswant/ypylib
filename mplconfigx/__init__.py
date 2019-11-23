9d3a13b8597894fce4dcda51a0ef60dc0926649c
yaswant <p_yaswant@yahoo.com>
2019-11-23 22:33:07 +0000

    add verbose for mplconfigx

--- mplconfigx/__init__.py
+++ mplconfigx/__init__.py
@@ -15,22 +15,28 @@ __author__ = "Yaswant Pradhan"
 __copyright__ = "(c) Crown copyright 2019, the Met Office."
 
 
-def loadrc():
+def loadrc(verbose=False):
     # - automatically switch backend to Agg if using a non-interactive or
     #   system with no DISPLAY environment
     if platform.startswith('linux'):
         if 'spice' in socket.gethostname() or os.getenv('DISPLAY') is None:
             mpl.use('Agg')
-    print('[info] backend: ', mpl.get_backend())
 
     # - avoid backend confilct
     mpl.rcParams["backend_fallback"] = True
 
     # - use custom sans and monospace fonts
     #   if not available, fallback to default
-    mpl.rcParams["font.sans-serif"].insert(0, "Fira Sans")
+    mpl.rcParams["font.sans-serif"] = [
+        "Open Sans", "Fira Sans"] + mpl.rcParamsDefault["font.sans-serif"]
     mpl.rcParams["font.monospace"] = [
         "Consolas", "Fira Mono"] + mpl.rcParamsDefault["font.monospace"]
+
+    if verbose:
+        print('backend:', mpl.get_backend(), "\n"
+              'backend_fallback:', mpl.rcParams["backend_fallback"], "\n"
+              'font.sans-serif:', mpl.rcParams["font.sans-serif"][0], "\n"
+              'font.monospace:', mpl.rcParams["font.monospace"][0])
 
 
 def unloadrc():
