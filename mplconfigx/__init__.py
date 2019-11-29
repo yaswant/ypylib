9094e5ee4e8bcfb3d3ce81f1ae38beb162278bb3
yaswant <p_yaswant@yahoo.com>
2019-11-29 16:54:38 +0000

    add default family as fallback

--- mplconfigx/__init__.py
+++ mplconfigx/__init__.py
@@ -5,17 +5,17 @@
 Custom matplotlib configuration
 
 """
+import matplotlib as mpl
 from sys import platform
 import os
 import socket
-import matplotlib as mpl
 
 __version__ = "0.1"
 __author__ = "Yaswant Pradhan"
 __copyright__ = "(c) Crown copyright 2019, the Met Office."
 
 
-def loadrc(verbose=False):
+def load():
     # - automatically switch backend to Agg if using a non-interactive or
     #   system with no DISPLAY environment
     if platform.startswith('linux'):
@@ -25,21 +25,20 @@ def loadrc(verbose=False):
     # - avoid backend confilct
     mpl.rcParams["backend_fallback"] = True
 
-    # - use custom sans and monospace fonts
-    #   if not available, fallback to default
-    mpl.rcParams["font.sans-serif"] = [
-        "Open Sans", "Fira Sans"] + mpl.rcParamsDefault["font.sans-serif"]
-    mpl.rcParams["font.monospace"] = [
-        "Consolas", "Fira Mono"] + mpl.rcParamsDefault["font.monospace"]
+    # -- font (only ttf, afm), grid
+    def_sans = mpl.rcParamsDefault['font.sans-serif']
+    def_mono = mpl.rcParamsDefault['font.monospace']
+    font = {
+        'family': 'sans-serif',
+        'sans-serif': ['Helvetica', 'Arial', 'Fira Sans'] + def_sans,
+        'monospace': ['Consolas', 'Courier New', 'Fira Mono'] + def_mono,
+    }
+    mpl.rc('font', **font)
+    mpl.rc('axes', grid=True, axisbelow=True)
+    mpl.rc(('xtick', 'ytick'), direction='in')
+    mpl.rc('lines', markersize=5)
 
-    if verbose:
-        print('backend:', mpl.get_backend(), "\n"
-              'backend_fallback:', mpl.rcParams["backend_fallback"], "\n"
-              'font.sans-serif:', mpl.rcParams["font.sans-serif"][0], "\n"
-              'font.monospace:', mpl.rcParams["font.monospace"][0])
 
-
-def unloadrc():
-    mpl.rcParams["backend_fallback"] = mpl.rcParamsDefault["backend_fallback"]
-    mpl.rcParams["font.sans-serif"] = mpl.rcParamsDefault["font.sans-serif"]
-    mpl.rcParams["font.monospace"] = mpl.rcParamsDefault["font.monospace"]
+def reset():
+    """Load matplotlib default rc parameters"""
+    mpl.rcdefaults()
